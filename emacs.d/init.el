570d61cb2ec9a0088da50af368fa4c9b9c27e129
wangjz <sochooligan@163.com>
2019-02-15 11:15:54 +0800

    update

---
+++ emacs.d/init.el
@@ -0,0 +1,18 @@
+
+;2019年 02月 15日 星期五 11:14:00 CST
+; ~/.emacs.d/
+
+(global-linum-mode 1) ; always show line numbers
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
